2bd05a70a127808dfa09f4e2fe3b6289ebfc0d47
souza.leandror@gmail.com <souza.leandror@gmail.com>
2019-09-02 19:06:58 -0300

    Initial commit

---
+++ lib/HomeWidget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Responsividade"),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
